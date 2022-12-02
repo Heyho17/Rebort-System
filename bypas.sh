f6c1bf3bf251798499a1df4e26e54fedde267014
ClassyBastardCyber <117260932+Heyho17@users.noreply.github.com>
2022-12-03 03:08:36 +0700

    Add files via upload

---
+++ bypas.sh
@@ -0,0 +1,280 @@
+#!bin/bash
+#version 2.0
+
+#MAU NGAPAIN TOT?
+#RECODE?REEDIT?RECOPYRIGHT?COPAS?
+#MALU TOLOL!!!
+#KAGA BERMORAL!
+#GUA DOA'IN YG COPYRIGHT COPAS RECORD TANPA IDZIN
+#BISUL GK SEMBUH SEMBUH.
+#AAMIIN
+#BIKIN SCRIPT INI PUYENG ANJING
+#GAUSAH ASAL RECORDE COPYRIGHT COPAS TANPA IDZIN KE GUA!!
+#MASIH MENDING GUA KASIH GRATISAN LU MONYET!
+#KALO MAU COPYRIGHT COPAS RECORD EDIT AUTHOR
+#IDZIN DULU KE FACEBOOK ADIK GUA!
+#MORE INFO DM FACEBOOK : ANISSA SYAHPUTRI
+
+#17/08/18
+
+clear
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+clear
+
+# tampilan
+clear
+toilet -f big -F gay LOGIN
+#pasword
+        read -p "[•]Masukan Username : " username
+        read -p "[•]Masukan Pasword : " pass
+echo $pass $username
+sleep 1
+echo
+if [ $pass = "####" ]
+        then
+        echo [•]"Pasword Salah"
+        sleep 2
+clear
+        else
+        echo -e $red [•]" Pasword Benar"
+        echo -e $red [•]" SELAMAT :V "
+        sleep 3
+        echo -e $red [•]" TAPI BOHONG :P"
+        sleep 4
+        sh bypas.sh
+        sleep 5
+fi
+sleep 2
+echo "SUBSCRIBE DULU YH KAWAN 🙏" | lolcat
+echo "JANGAN SAMPE ENGGA 😔"
+echo "HARGAI KARYA KAMI DENGAN CARA"
+echo "SUBSCRIBE YOUTUBE KAMI 🙏" | lolcat
+sleep 6
+
+xdg-open https://www.youtube.com/@Heyho-Cyber
+sleep 7
+
+# tampilan
+clear
+sleep 3
+toilet -f mini -F gay Bypas System
+sleep 3
+echo -e $red "[•]____________________________________[•]"
+echo -e $red "[•]         REBORT SYSTEM ANDROID      [•]"
+echo -e $g  "[•]====================================[•]"
+echo -e $g  "[•]         -0              0-         [•]"
+echo -e $g  "[•]          +hydNNNNNNNNdyh+          [•]"
+echo -e $g  "[•]         +nmmmmmmmmmmmmmmn+         [•]"
+echo -e $g  "[•]      'dMMm::NmmmnmmmN::mMNd'       [•]"
+echo -e $g  "[•]       hMMMMMMMMMMMMMMMMMMMMh       [•]"
+echo -e $g  "[•]  ••  _yyyyyyyyyyyyyyyyyyyyyy_  ••  [•]"
+echo -e $g  "[•] _nMMn MMMMMMM_______MMMMMMMM nMMn_ [•]"
+echo -e $g  "[•]:MMMMM_MMMMMM/       \MMMMMMM_MMMM: [•]"
+echo -e $g  "[•]:MMMMM_MMMMMM|EROR.. |MMMMMMM_MMMM: [•]"
+echo -e $g  "[•]:MMMMM_MMMMMM| 404   |MMMMMMM_MMMM: [•]"
+echo -e $g  "[•]:MMMMM_MMMMMM|  !    |MMMMMMM_MMMM: [•]"
+echo -e $g  "[•]:MMMMM_MMMMMM|EROR.. |MMMMMMM_MMMM: [•]"
+echo -e $g  "[•] +yy+  MMMMMM\_______/MMMMMMM  +yy+ [•]"
+echo -e $g  "[•]       mMMMMMMMMMMMMMMMMMMMm        [•]"
+echo -e $g  "[•]       '/++MMMMh++hMMMM++/'         [•]"
+echo -e $g  "[•]           MMMMo  oMMMM             [•]"
+echo -e $g  "[•]           MMMMo  oMMMM             [•]"
+echo -e $g  "[•]           oNMm-  -mMNo             [•]"
+echo -e $red "[•]____________________________________[•]"
+echo -e $red "[•]TYPE     : TOOLS BYPAS              [•]"
+echo -e $red "[•]VERSION  : 0.0                      [•]"
+echo -e $red "[•]AUTHOR   : MR.HEYHO                 [•]"
+echo -e $red "[•]FACEBOOK : ANISSA SYAHPUTRI         [•]"
+echo -e $red "[•]TEAM     : CLASSY BASTARD CYBER     [•]"
+echo -e $red "[•]####################################[•]"
+sleep 3
+echo "[•]_______________NOTES_______________________[•]"
+echo "[•]    PASTIKAN HP ANDA SUDAH DI RISET        [•]" | lolcat
+echo "[•]             WIPE ALL DATA                 [•]" | lolcat
+echo "[•]             REBORT SYSTEM                 [•]" | lolcat
+echo "[•]    INI HANYA CARA BYPAS SECARA MANUAL     [•]" | lolcat
+echo "[•]        SILAHKAN IKUTI YG DI KASIH         [•]" | lolcat
+echo "[•]             TANDA CEKLIS                  [•]" | lolcat
+echo "[•]          SEMOGA BERMANFAAT                [•]"
+echo "[•]###########################################[•]" | lolcat
+sleep 3
+echo "[•]_________________MIUI.11___________________[•]"
+echo "[•] <MENU>                                    [•]" | lolcat
+echo "[ENTER TO MENU]                               [•]" | lolcat
+echo "#################################################" | lolcat
+read -p "[ENTER]"
+sleep 3
+echo "[•]_______________PILIH BAHASA________________[•]"
+echo "[•] INDONESIA [✓]                             [•]" | lolcat
+echo "[•] ENGLISH "UNITED KINGDOM"                    [•]" | lolcat
+echo "[•] AZARBAYCAN                                [•]" | lolcat
+echo "[•] MELAYU                                    [•]" | lolcat
+echo "[•] BOSANSKI                                  [•]" | lolcat
+echo "[•] CATAlA                                    [•]" | lolcat
+echo "[•] CESTINA                                   [•]" | lolcat
+echo "[•] DANSK                                     [•]" | lolcat
+echo "[ENTER TO NEXT]                               [•]" | lolcat
+echo "[•]###########################################[•]" | lolcat
+read -p "[ENTER]"
+sleep 3
+echo "[•]_____________PASANG SIM CARD_______________[•]"
+echo "[•] SIM 1 [✓]                                 [•]" | lolcat
+echo "[•] SIM 2 [✓]                                 [•]" | lolcat
+echo "[•]<BEBAS PAKAI KARTU BERAPAPUN>              [•]" | lolcat
+echo "[ENTER TO NEXT]                               [•]" | lolcat
+echo "#################################################" | lolcat
+read -p "[ENTER]"
+sleep 3
+
+echo "[•]________________PILIH LOKASI_______________[•]"
+echo "[•] INDONESIA [✓]                             [•]" | lolcat
+echo "[•] AFGHANISTAN                               [•]" | lolcat
+echo "[•] AFRIKA SELATAN                            [•]" | lolcat
+echo "[•] ALBANIA                                   [•]" | lolcat
+echo "[•] ALJAZAIR                                  [•]" | lolcat
+echo "[•] AMERIKA SERIKAT                           [•]" | lolcat
+echo "[•] ANDORRA                                   [•]" | lolcat
+echo "[ENTER TO NEXT]                               [•]" | lolcat
+echo "[•]###########################################[•]" | lolcat
+read -p "[ENTER]"
+sleep 3
+echo "[•]_______________JARINGAN WIFI_______________[•]"
+echo "[•]   <SAMBUNGKAN KEPADA WIFI YG AKTIF> [✓]   [•]" | lolcat
+echo "[NEXT TO ENTER]                               [•]" | lolcat
+echo "#################################################" | lolcat
+read -p "[ENTER]"
+sleep 3
+echo "[•]____________SYARAT DAN KETENTUAN___________[•]"
+echo "[•] <SYARAT DAN KETENTUAN> [✓]                [•]" | lolcat
+echo "[ENTER TO NEXT]                               [•]" | lolcat
+echo "#################################################" | lolcat
+read -p "[ENTER]"
+sleep 3
+echo "[•]___________SALIN APLIKASI & DATA___________[•]"
+echo "[•] <JANGAN SALIN> [✓]                        [•]" | lolcat
+echo "[ENTER TO NEXT]                               [•]" | lolcat
+echo "#################################################" | lolcat
+read -p "[ENTER]"
+sleep 3
+echo "[•]__________________GAMBAR POLA______________[•]"
+echo "[•] <LUPA POLA>    o  o  o                    [•]" | lolcat
+echo "[•]    KLIK |      o  o  o                    [•]" | lolcat
+echo "[•]         V      o  o  o                    [•]" | lolcat
+echo "[•] <GUNAKAN AKUN GOOGLE SAYA> [✓]            [•]" | lolcat
+echo "[ENTER TO NEXT]                               [•]" | lolcat
+echo "#################################################" | lolcat
+read -p "[ENTER]"
+sleep 3
+echo "[•]_________________GOOGLE____________________[•]"
+echo "[•]          VERIFIKASI AKUN ANDA             [•]" | lolcat
+echo "[•]       Menggunakan Akun Google Anda        [•]" | lolcat
+echo "[•]                    |                      [•]"
+echo "[•]                    V                      [•]"
+echo "[•]EMAIL : BAGI YG INGAT SAJA                 [•]" | lolcat
+echo "[•]PASSWORD : BAGI YG INGAT SAJA              [•]" | lolcat
+echo "[•]            NOTES |                        [•]"
+echo "[•]                  V                        [•]"
+echo "[•]     BAGI YG LUPA AKUN GOOGLE NYA          [•]" | lolcat
+echo "[•]       SILAHKAN IKUTI CARA DI |            [•]"
+echo "[•]                              V            [•]"
+echo "[ENTER TO NEXT]       <=========              [•]" | lolcat
+echo "#################################################" | lolcat
+read -p "[ENTER]"
+sleep 3
+echo "[•]___________KEMBALI KE JARINGAN WIFI________[•]"
+echo "[•]    <SAMBUNGKAN KEPADA WIFI YG AKTIF> [✓]  [•]" | lolcat
+echo "[•]           <KEMUDIAN klik yg> |            [•]"
+echo "[•]                              V            [•]"
+echo "[•]          <TAMBAHKAN JARINGAN>   [✓]       [•]" | lolcat
+echo "[•] [SSID              www.YouTube.com]       [•]" | lolcat
+echo "[•]                          ⬆️                [•]"
+echo "[•]                      [TEKAN LAMA]         [•]" | lolcat
+echo "[•]  >KEMUDIAN<                               [•]" | lolcat
+echo "[•] <KLIK GAMBAR>                             [•]" | lolcat
+echo "[•]   SEPERTI DI |                            [•]"
+echo "[•]              V                            [•]"
+echo "[•]             📤                            [•]"
+echo "[•]                         <DAN PILIH PESAN> [•]" | lolcat
+echo "[•]                            >KEMUDIAN ⬇️    [•]"
+echo "[ENTER TO NEXT]       <=================      [•]" | lolcat
+echo "#################################################" | lolcat
+read -p "[ENTER]"
+sleep 3
+echo "[•]__________________KIRIM SMS________________[•]"
+echo "[•] <Www.YouTube.com> [✓]                     [•]" | lolcat
+echo "[•]  <KIRIM KEPADA> ⬇️                         [•]"
+echo "[•]             <234>  [✓]                    [•]" | lolcat
+echo "[•]    <TEKAN PESAN YG DI KIRIM>              [•]" | lolcat
+echo "[•]     PILIH <TELUSURI YOUTUBE> [✓]          [•]" | lolcat
+echo "[ENTER TO NEXT]                               [•]" | lolcat
+echo "#################################################" | lolcat
+read -p "[ENTER]"
+sleep 3
+echo "[•]_________________YOUTUBE___________________[•]"
+echo "[•] <OK> [✓]                                  [•]" | lolcat
+echo "[•]          <KEMUDIAN KLIK> ⬇️                [•]"
+echo "[•]              <👤AKUN> [✓]                 [•]"
+echo "[•]               <STELAN> [✓]                [•]" | lolcat
+echo "[•]          <HISTORY & PRIVASI> [✓]          [•]" | lolcat
+echo "[•]      <PERSYARATAN LAYANAN YOUTUBE> [✓]    [•]" | lolcat
+echo "[•]         <SETUJU & LANJUTKAN> [✓]          [•]" | lolcat
+echo "[•] <Browser>⬇️                                [•]"
+echo "[•]  [ vnrom Bypass ] [✓]                     [•]" | lolcat
+echo "[•] <KLIK YG NO 1>                            [•]" | lolcat
+echo "[ENTER TO NEXT]                               [•]" | lolcat
+echo "#################################################" | lolcat
+read -p "[ENTER]"
+sleep 3
+echo "[•]_______________BYPAS ANDROID_______________[•]"
+echo "[•] INSTALL <Android_8-9_gam.apk> [✓]         [•]" | lolcat
+echo "[•] INSTALL <FRP_Bypass.apk>      [✓]         [•]" | lolcat
+echo "[ENTER TO NEXT]                               [•]" | lolcat
+echo "#################################################" | lolcat
+read -p "[ENTER]"
+sleep 3
+echo "[•]_____________MENJALANKAN APK BYPAS_________[•]"
+echo "[•] FRP_Bypass.apk [OPEN] [✓]                 [•]" | lolcat
+echo "[•] ANDROID_8-9_GAM.apk [CLOSED] [X]          [•]" | lolcat
+echo "[•]              ⬇️                            [•]" | lolcat
+echo "[•] <KLIK TITIK 3 DI POJOK KANAN ATAS>        [•]" | lolcat
+echo "[•] <MASUK BROWSER>  [✓]                      [•]" | lolcat
+echo "[•] <OK> [✓]                                  [•]" | lolcat
+echo "[ENTER TO NEXT]                               [•]" | lolcat
+echo "#################################################" | lolcat
+read -p "[ENTER]"
+sleep 3
+echo "[•]___________________GOOGLE__________________[•]"
+echo "[•]       Login Dengan Akun Google Anda       [•]" | lolcat
+echo "[•]                     👤                    [•]"
+echo "[•]     Email : Bagi Yg punya Akun Tumbal     [•]" | lolcat
+echo "[•]     Password : Bagi Yg Punya Akun Tumbal  [•]" | lolcat
+echo "[•]                     ⬇️                     [•]" | lolcat
+echo "[•]      <BAGI YG BELUM PUNYA AKUN TUMBAL<    [•]" | lolcat
+echo "[•]           <langsung Saja KLIK ⬇️           [•]" | lolcat
+echo "[•]                         | <BUAT AKUN> |   [•]" | lolcat
+echo "[•]                         |_____________|   [•]" | lolcat
+echo "[•] <Setelah Berhasil Buat Akun Baru>         [•]" | lolcat
+echo "[•] <Langsung Deh Login Dan Selesai>          [•]" | lolcat
+echo "[•]______________SELESAI DEH__________________[•]" | lolcat
+echo -e $red "[•] Awas Jangan Sampai Lupa Lagi [•]"
+echo -e $red "[•]  Email & Password nya 🤣     [•]"
+echo "[•]###########################################[•]" | lolcat
+sleep 3
+echo "[•] ______________ALHAMDULILAH________________[•]"
+echo "[•] <Mr.Heyho! 🎭                             [•]"
+echo "[•]##############################################" | lolcat
